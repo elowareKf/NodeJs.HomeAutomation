193637d2df577007cd29e856f260142a51928ca2
KFischer (mba) <klaus.fischer@eloware.com>
2019-11-17 13:00:49 +0100

    Removed unused packages

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
@@ -16,14 +15,11 @@ app.use(bodyParser.json());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/api/revolution', revolutionRouter);
 
 // catch 404 and forward to error handler
